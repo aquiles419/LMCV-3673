f53d7b18e8b672fb67ccf5c5baa6dd4f2f6f086c
Aquiles <aquiles@MacBook-Pro-de-Aquiles.local>
2022-02-15 10:45:27 -0300

    teste

--- app-mongo/src/app/controller/userController.js
+++ app-mongo/src/app/controller/userController.js
@@ -27,20 +27,6 @@ class UserController {
 
       await producer.disconnect()
 
-      //Consumer 
-      const consumer = kafka.consumer({ groupId: 'test-group' })
-
-      await consumer.connect()
-      await consumer.subscribe({ topic: 'test-topic', fromBeginning: true })
-
-      await consumer.run({
-        eachMessage: async ({ topic, partition, message }) => {
-          console.log({
-            value: message.value.toString(),
-          })
-        },
-      })
-
       //end userController
     return res.json(data);
   }
